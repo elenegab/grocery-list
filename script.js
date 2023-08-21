a7efaccac746916da50693f64706eec2a2e00a45
Elene <gabidaidze1234@gmail.com>
2023-08-21 22:31:27 +0400

    grocery list

---
+++ script.js
@@ -0,0 +1,28 @@
+const pencil = document.getElementById("pencil");
+const allItems = document.getElementById("allItems");
+const userInput = document.getElementById("userInput");
+
+pencil.addEventListener("click", () => {
+  allItems.innerHTML = "";
+
+});
+
+userInput.addEventListener("keydown", (event) =>{
+  if(event.key == "Enter" & userInput.value != ""){
+    addItem();
+  }
+});
+
+function addItem(){
+  var h2 = document.createElement("h2");
+  h2.innerHTML = "-" + userInput.value;
+
+  h2.addEventListener("click" ,() => {
+    h2.style.textDecoration = "line-through";
+  });
+
+  allItems.insertAdjacentElement("beforeend", h2);
+  userInput.value = "";
+
+}
+ 
